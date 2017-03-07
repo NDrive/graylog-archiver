eec9074f10c9dd46c5ec01fd02b7b0e0cf7ec849
André Freitas <andre.freitas@ndrive.com>
2017-03-07 14:58:35 +0000

    fix first preview version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Graylog Archiver',
-    version='1.0',
+    version='0.1.0',
     description='Archives Graylog closed indices with rsync.',
     entry_points={"console_scripts": ['graylog-archiver = graylog_archiver.cli:main']}, #noqa
     author='André Freitas',
